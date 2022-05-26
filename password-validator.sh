4a787a6fa0cf9baf5b25bb8dd68087841c48f7e6
johnmogi <anguru@gmail.com>
2022-05-26 19:50:03 +0300

    fix filepath

--- password-validator.sh
+++ password-validator.sh
@@ -10,15 +10,14 @@ Red='\033[0;31m'        # Red
 Green='\033[0;32m'      # Green
 NC='\033[0m '           # No Color
 
-# exit the program if no argument supplied:
-if [ ${#1} -lt $min_length ]; then
-printf "${Red}Try again... \nPassword on file is too short, there should be at least $min_length characters${NC}\n"
-exit 1;
-fi
-
 # validate function tests password string for complexity: 
 # to ensure exit code I'm using echo insted of plain checks so that exit codes will return upon each check.
 validate (){ 
+if [ ${#1} -lt $min_length ]; then
+    printf "${Red}Try again... \nPassword must contain at least $min_length characters${NC}\n"
+    exit 1
+fi
+
 echo "$1" | grep -Eq '.*[0-9]' 
 if [ $? -eq 1 ] ; then
     printf "${Red}Try again... \nPassword must contain at least one number${NC}\n"
@@ -38,7 +37,26 @@ if [ $? -eq 1 ] ; then
 fi
 }
 
+# check if the -f flag is set, if it does, run validate on password file.
+# exit the program if no argument supplied
+case $1 in
+"-f" )
+file=$2
+# password_from_file=$(<$2) not working on some tests! trying the following instead of getopst:
+while IFS= read -r password_from_file; do
+    printf '%s\n' "$password_from_file"
+done < "$file"
+if [ ${#password_from_file}  ] ; then
+validate $password_from_file
+else
+printf "${Red} Please specify a filepath along the -f <'./filepath>${NC}\n"
+exit 1
+fi
+;;
+*)
 validate $1
+;;
+esac 
 
 printf "${Green}success${NC}password is secured\n"
 exit 0
