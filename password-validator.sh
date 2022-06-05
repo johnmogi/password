4994864db0e9a7f2fa4d64f50afd9c8b7a59a8b1
johnmogi <anguru@gmail.com>
2022-06-05 12:21:42 +0300

    fix

--- password-validator.sh
+++ password-validator.sh
@@ -9,38 +9,41 @@ min_length=1 # avoiding magic number to set minimum amount of characters.
 Red='\033[0;31m'        # Red 
 Green='\033[0;32m'      # Green
 NC='\033[0m '           # No Color
+valid=1
 
 # validate function tests password string for complexity: 
 # to ensure exit code I'm using echo insted of plain checks so that exit codes will return upon each check.
-validate (){ 
+
 if [ ${#1} -lt $min_length ]; then
-    printf "${Red}Try again... \nPassword must contain at least $min_length characters${NC}\n"
-    exit 1
-fi
-
-if [[ ! $1 =~ '.*[0-9]' ]]; then
-    printf "${Red}Try again... \nPassword must contain at least one number${NC}\n"
+    printf "${Red}Password must contain at least $min_length characters${NC}\n"
     exit 1;
 fi
-if [[  $1 =~ '.*[a-z]' ]]; then
-     printf "${Red}Try again... \nPassword must contain at least one small letter${NC}\n"
-    exit 1;
-fi
-if [[  $1 =~ '.*[A-Z]' ]]; then
-     printf "${Red}Try again... \nPassword must contain at least one capital letter${NC}\n"
-    exit 1;
 
-# echo "$1" | grep -Eq '.*[a-z]' 
-# if [ $? -eq 1 ] ; then
-#     printf "${Red}Try again... \nPassword must contain at least one small letter${NC}\n"
-#     exit 1
-# fi
+validate (){ 
 
-# echo "$1" | grep -Eq '.*[A-Z]'
-# if [ $? -eq 1 ] ; then
-#     printf "${Red}Try again... \nPassword must contain at least one capital letter${NC}\n"
-#     exit 1
+if [[ $1 =~ [0-9] ]]; then
+    valid=0
+else
+    printf "${Red}Password must contain at least one number${NC}\n"
 fi
+
+if [[ $1 =~ [a-z] ]]; then
+    valid=0
+else
+     printf "${Red}Password must contain at least one small character${NC}\n"
+    valid=1
+fi
+
+if [[ $1 =~ [A-Z] ]]; then
+    valid=0
+else
+     printf "${Red}Password must contain at least one capital character${NC}\n"
+     valid=1
+fi
+
+
+echo $valid
+
 }
 
 # check if the -f flag is set, if it does, run validate on password file.
@@ -52,6 +55,7 @@ file=$2
 while IFS= read -r password_from_file; do
     printf "$password_from_file"
 done < "$file"
+
 if [ $? -eq 0 ] ; then
 validate $password_from_file
 else
@@ -64,5 +68,12 @@ validate $1
 ;;
 esac 
 
+
+if [[ $valid -eq 1 ]]; then
+     printf "${Red} Try again... ${NC}\n"
+    exit 0
+    else
 printf "${Green}success${NC}password is secured\n"
+
+fi
 exit 0
