037f1ae87c2a36fe6a9f2eb52a5fa1c91de92594
johnmogi <anguru@gmail.com>
2022-08-14 17:36:31 +0300

    up

--- password-validator.py
+++ password-validator.py
@@ -6,11 +6,16 @@ min_length=2
 p1 =  str(sys.argv[1])
 
 if len(p1) < min_length:
-    printf('Try again... \nPassword on file is too short, there should be at least ${min_length} characters')
-# else:
-#     print('the password is ok')
+    print(f'Try again... \n {Fore.RED}Password on file is too short,{Style.RESET_ALL} there should be at least {min_length} characters')
+
+def validate(param):
+    a = any(chr.isdigit() for chr in param)
+    print(a)
+
+validate(p1)
+
+print(f'success: \n {Fore.GREEN} password is secure{Style.RESET_ALL} ')
 
 
-print(f"The password {Fore.GREEN}is ok{Style.RESET_ALL}!")
 
 # if min_length <
